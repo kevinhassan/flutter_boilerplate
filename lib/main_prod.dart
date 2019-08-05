5188663bd6617d66d4b4611a418ae5d207d4d622
kevinhassan <kevin.hassan@idean.com>
2019-08-05 18:04:05 +0200

    🐛🎨 - Fixing http error status enum

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-
-import 'config.dart';
+import 'package:flutter_boilerplate/src/config/config.dart';
 import 'main.dart';
 
 void main() {
   var configuredApp = new Config(
-    appName: 'DeviceBoard',
-    environment: 'Production',
+    environment: Environment.PRODUCTION,
     apiBaseUrl: 'http://example.com:8000',
     child: new MyApp(),
   );
--- lib/src/utils/network.dart
+++ lib/src/utils/network.dart
@@ -5,6 +5,10 @@ import 'package:http/http.dart' as http;
 class NetworkUtil {
   static final NetworkUtil _instance = new NetworkUtil.internal();
   static String apiBaseUrl;
+  final JsonDecoder _decoder = new JsonDecoder();
+
+  //TODO: store token in securise location
+  String _token = "";
 
   NetworkUtil.internal();
   factory NetworkUtil(String apiUrl) {
@@ -12,44 +16,128 @@ class NetworkUtil {
     return _instance;
   }
 
-  final JsonDecoder _decoder = new JsonDecoder();
-
   Future<dynamic> get(String resourcePath,
-      [Map<String, String> queryParameters]) async {
-    final uri = _buildUri(resourcePath, queryParameters);
-    final response = await http.get(uri);
-    final String res = response.body;
-    final int statusCode = response.statusCode;
-    if (statusCode < 200 || statusCode > 400 || json == null) {
-      throw new Exception("Error while fetching data");
+      [Map<String, String> queryParameters,
+      bool isAuthenticated = false]) async {
+    try {
+      final headers = {"Authorization": "Bearer $_token"};
+      final uri = _buildUri(resourcePath, queryParameters);
+      final response = _token.isNotEmpty && isAuthenticated
+          ? await http.get(uri, headers: headers)
+          : await http.get(uri);
+      refreshToken(response);
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+      if (statusCode >= 400) throw HttpException(statusCode);
+      return _decoder.convert(res);
+    } catch (exception) {
+      return Future.error(exception);
     }
-    return _decoder.convert(res);
   }
 
   Future<dynamic> post(String resourcePath,
-      {Map headers, body, encoding}) async {
-    final uri = _buildUri(resourcePath);
-    final response =
-        await http.post(uri, body: body, headers: headers, encoding: encoding);
-    final String res = response.body;
-    final int statusCode = response.statusCode;
-
-    if (statusCode < 200 || statusCode > 400 || json == null) {
-      throw new Exception("Error while fetching data");
+      {Map headers,
+      Map body,
+      Encoding encoding,
+      bool isAuthentificated = true}) async {
+    try {
+      if (_token.isNotEmpty) {
+        headers["Authorization"] = "Bearer $_token";
+      }
+      final uri = _buildUri(resourcePath);
+      final response = await http.post(uri,
+          body: body, headers: headers, encoding: encoding);
+      refreshToken(response);
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+      if (statusCode >= 400) throw HttpException(statusCode);
+      return _decoder.convert(res);
+    } catch (exception) {
+      return Future.error(exception);
     }
-    return _decoder.convert(res);
+  }
+
+  Future<dynamic> put(String resourcePath,
+      {Map headers,
+      Map body,
+      Encoding encoding,
+      bool isAuthentificated = true}) async {
+    try {
+      if (_token.isNotEmpty) {
+        headers["Authorization"] = "Bearer $_token";
+      }
+      final uri = _buildUri(resourcePath);
+      final response =
+          await http.put(uri, body: body, headers: headers, encoding: encoding);
+      refreshToken(response);
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+      if (statusCode >= 400) throw HttpException(statusCode);
+      return _decoder.convert(res);
+    } catch (exception) {
+      return Future.error(exception);
+    }
   }
 
   Uri _buildUri(String resourcePath, [Map<String, String> queryParameters]) {
     return Uri.parse('$apiBaseUrl/$resourcePath')
       ..replace(queryParameters: queryParameters);
   }
+
+  void resetToken() {
+    _token = "";
+  }
+
+  void refreshToken(http.Response response) {
+    final newToken = response.headers["Authorization"];
+    if (newToken?.isNotEmpty ?? false) {
+      _token = newToken;
+    }
+  }
 }
 
-enum HttpError {
-  Unauthorized,
+enum HttpStatusError {
+  UnAuthorized,
   BadRequest,
-  NotFound,
   Forbidden,
-  InternalServerError
+  NotFound,
+  InternalServerError,
+  Unknown
+}
+
+class HttpException implements Exception {
+  HttpStatusError status;
+  String message;
+  HttpException(int statusCode) {
+    switch (statusCode) {
+      case 400:
+        this.status = HttpStatusError.BadRequest;
+        this.message = "Bad request";
+        break;
+      case 401:
+        this.status = HttpStatusError.UnAuthorized;
+        this.message = "UnAuthorized access ";
+        break;
+      case 403:
+        this.status = HttpStatusError.Forbidden;
+        this.message = "Resource access forbidden";
+        break;
+      case 404:
+        this.status = HttpStatusError.NotFound;
+        this.message = "Resource not Found";
+        break;
+      case 500:
+        this.status = HttpStatusError.InternalServerError;
+        this.message = "Internal server error";
+        break;
+      default:
+        this.status = HttpStatusError.Unknown;
+        this.message = "Unknown";
+        break;
+    }
+  }
+  String toString() {
+    if (message == null) return "Exception";
+    return message;
+  }
 }
